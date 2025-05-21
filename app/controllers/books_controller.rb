0e900d00796199ca84ce7934744e9b04e7455fad
k-tomita20 <208903862+k-tomita20@users.noreply.github.com>
2025-05-21 09:51:52 +0000

    [Update] ルートパスの更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,8 +30,12 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice]
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
